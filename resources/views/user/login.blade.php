ee0624ad18653cff8b137259bf546bf83869ad7b
dorumamu <64189518+dorumamu@users.noreply.github.com>
2020-05-11 16:12:43 +0900

    1차 완성
    1차 완성본

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -9,20 +9,28 @@
 
 @section('content')
     <div class="login_layout">
-        <img src="/img/main_img.png"  alt="로고" class="center-block">
       <div class="login_form">
         <form>
-          <label>ID(아이디)<br>
-            <input type="text" name="id" id="id" placeholder="ID입력(필수)" required><br>
-          </label>
-
-          <label>Password(비밀번호)<br>
-            <input type="text" name="PW" id="pw" placeholder="비밀번호" required><br>
-          </label>
-           <button type="submit" id="login_bt" name="login" onclick="return to_submit();">로그인</button>
-
-           <a href="#">ID/Password 찾기</a>
-           <a href="/sign_up">회원가입</a>
+          <ul>
+            <li>
+              <img src="/img/main_img.png"  alt="로고" class="center-block">
+            </li>
+            <li>
+              <label><strong>ID(아이디)</strong><br>
+                <input type="text" name="id" id="id" placeholder="ID입력(필수)" required><br>
+              </label>
+            </li>
+            <li>
+              <label><strong>Password(비밀번호)</strong><br>
+                <input type="text" name="PW" id="pw" placeholder="비밀번호" required><br>
+              </label>
+            </li>
+            <li>
+            <button type="submit" id="login_bt" name="login" onclick="return to_submit();">로그인</button>
+          </li>
+          <li>
+            <a href="#">ID/Password 찾기</a>
+            <a href="/sign_up">회원가입</a>
         </form>
       </div>
     </div>
