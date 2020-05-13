08a30c7ea95caffe5cb223c4b0897a4e759ccf70
dorumamu <64189518+dorumamu@users.noreply.github.com>
2020-05-13 21:55:50 +0900

    main 제작중
    메인페이지 제작중

---
+++ resources/views/main.blade.php
@@ -0,0 +1,24 @@
+@extends('layout.layout_main')
+
+@section('title')
+  대한민국 No.1 중고땅땅!
+@endsection
+
+@section('css')
+  <link rel="stylesheet" href="/css/main.css">
+@endsection
+
+@section('content')
+  <div class="article">
+    <div class="form">
+      <div class="pagination">
+        <a href="#">«</a>
+        <a href="#">1</a>
+        <a href="#">2</a>
+        <a href="#">3</a>
+        <a href="#">4</a>
+        <a href="#">»</a>
+      </div>
+    </div>
+  </div>
+@endsection
