3bb00f31b90f2d381b8320b685d4b414ef250759
GyeongNam <knightanne97@naver.com>
2020-05-28 10:18:17 +0900

    회원가입 확인

--- resources/views/login/sign_up.blade.php
+++ resources/views/login/sign_up.blade.php
@@ -16,7 +16,7 @@ var RandomNum;
 function mainsends(){
   var random = Math.floor(Math.random() * 10000) + 1;
   var email = $('#str_email01').val();
-  var email_domain = $('str_email02').val();
+  var email_domain = $('#str_email02').val();
   RandomNum = random;
   var mails = email+'@'+email_domain;
   $.ajax({
@@ -263,14 +263,13 @@ function chkpw(){
           </li>
           <script type="text/javascript">
            //이메일 입력방식 선택
-           $('#selectEmail').change(function()
-           { $("#selectEmail option:selected").each(function ()
-           { if($(this).val()== '1'){ //직접입력일 경우
+           $('#selectEmail').change(function(){
+             $("#selectEmail option:selected").each(function (){
+               if($(this).val()== '1'){ //직접입력일 경우
              $("#str_email02").val('');//값 초기화
              $("#str_email02").attr("disabled",false); //활성화
-
-
-            }else{    //직접입력이 아닐경우
+            }
+            else{    //직접입력이 아닐경우
             $("#str_email02").attr("disabled",true); //비활성화
             $("#str_email02").val($(this).text()); //선택값 입력
 
