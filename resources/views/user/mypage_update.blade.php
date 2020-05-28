e8c14b00451b2e578ea0ec55f9ae8ca8475371c1
GyeongNam <knightanne97@naver.com>
2020-05-28 13:18:32 +0900

    slsect input error disabled -> readonly

--- resources/views/user/mypage_update.blade.php
+++ resources/views/user/mypage_update.blade.php
@@ -22,10 +22,10 @@ $('#selectEmail').change(function()
 { if($(this).val()== '1'){ //직접입력일 경우
   $("#str_email02").val('');
   //값 초기화
-  $("#str_email02").attr("disabled",false); //활성화
+  $("#str_email02").attr("readonly",false); //활성화
 }else{    //직접입력이 아닐경우
   $("#str_email02").val($(this).text()); //선택값 입력
-  $("#str_email02").attr("disabled",true); //비활성화
+  $("#str_email02").attr("readonly",true); //비활성화
 }
 });
 });
