e39cdcf3aaac367815d0edf0b0d992a4c97c0e9a
GyeongNam <knightanne97@naver.com>
2020-06-02 14:35:10 +0900

    스케줄링 주기 설정

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('item:success_update')->everyMinute();
+        $schedule->command('item:success_update')->daily();
     }
 
     /**
