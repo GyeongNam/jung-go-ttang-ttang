c8f90d1484a3382a7cec38863a76d164f74cf9d7
GyeongNam <knightanne97@naver.com>
2020-06-02 13:20:48 +0900

    scheduling 실험

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('item:success_update')->daily();
+        $schedule->command('item:success_update')->everyMinute();
     }
 
     /**
