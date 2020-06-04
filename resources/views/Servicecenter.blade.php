38eb6fd2ea66f8b9b1439b2a133f1da778d5ee49
dorumamu <64189518+dorumamu@users.noreply.github.com>
2020-06-04 14:52:56 +0900

    서비스페이지 등록

---
+++ resources/views/Servicecenter.blade.php
@@ -0,0 +1,91 @@
+@extends('layout.layout_main')
+
+@section('css')
+<link rel="stylesheet" href="/css/servicesenter.css">
+@endsection
+
+@section('js')
+<script type="text/javascript">
+
+</script>
+
+@endsection
+@section('content')
+<div class="wish_list_page">
+  <div class="wish_content">
+    <div class="wish_header">
+      <div class="mylike">
+        내관심
+        <span class="mylikesu">2</span>
+      </div>
+      <hr></div>
+      <div class="wish_tag">
+        <div class="wish_btn">
+          <div class="w_chack">
+            <button type="button" name="button">V</button>
+            <button class="sel_del" id="sel_delbt"type="button" name="button" onclick="delete_select">선택 삭제</button>
+            <div class="hyper">
+              <a href="#">최신순</a>
+              <a href="#">높은가격순</a>
+              <a href="#">낮은가격순</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="wish_block">
+        <div class="wish_info">
+          <div class="wish_item_info">
+           <a href="#" class="prdt_link">
+             <div class="wish_item_img">
+             <img src="/img/item/릴카.png" alt="">
+             <div class="check_self">
+               <div class="check_qus nost divnost">
+                 ::before
+                 ::after
+               </div>
+             </div>
+           </div>
+           <div class="w_i_info">
+             <div class="w_i_pro_i">
+               <div class="wiproname">
+                 아이폰 프로 맥스
+               </div>
+               <div class="wiproprice">
+                 187,900원
+               </div>
+               <div class="wiprodate">
+                 5일전
+               </div>
+             </div>
+           </div>
+         </a>
+          </div>
+        </div>
+        <div class="wish_info">
+          <div class="wish_item_info">
+           <a href="#" class="prdt_link">
+             <div class="wish_item_img">
+             <img src="/img/item/릴카.png" alt="">
+           </div>
+           <div class="w_i_info">
+             <div class="w_i_pro_i">
+               <div class="wiproname">
+                 아이폰 프로 맥스
+               </div>
+               <div class="wiproprice">
+                 187,900원
+               </div>
+               <div class="wiprodate">
+                 5일전
+               </div>
+             </div>
+           </div>
+         </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="tung"></div>
+  </div>
+  @endsection
