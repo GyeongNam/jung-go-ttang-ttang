847b0a448dbf5da4d2931125d21b068ed0b797e9
dorumamu <64189518+dorumamu@users.noreply.github.com>
2020-06-09 15:50:04 +0900

    하드코딩부분 제거

--- resources/views/wish_list.blade.php
+++ resources/views/wish_list.blade.php
@@ -84,32 +84,9 @@ $(document).ready(function(){
               </div>
             </a>
           </div>
-          @endforeach
-        </div>
-        <div class="wish_info">
-          <div class="wish_item_info">
-            <a href="#" class="prdt_link">
-              <div class="wish_item_img">
-                <input type="checkbox" name="chk" class="chackSelect" value="">
-                <img src="/img/item/릴카.png" alt="">
-              </div>
-              <div class="w_i_info">
-                <div class="w_i_pro_i">
-                  <div class="wiproname">
-                    아이폰 프로 맥스
-                  </div>
-                  <div class="wiproprice">
-                    187,900원
-                  </div>
-                  <div class="wiprodate">
-                    5일전
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
         </div>
       </div>
+        @endforeach
     </div>
     <div class="tung"></div>
   </div>
