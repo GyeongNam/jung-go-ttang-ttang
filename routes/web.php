6689bc5eab97eeed9c9f09d127820469e157848e
GyeongNam <knightanne97@naver.com>
2020-06-15 13:48:30 +0900

    re 메일 비밀번호_4

--- routes/web.php
+++ routes/web.php
@@ -81,8 +81,8 @@ Route::get('/Logout', 'UserController@logout')->middleware('login');
 Route::post('/mypage_update', 'UserController@mypage_update')->middleware('login');
 Route::post('/idcheck', 'UserController@idcheck');
 Route::get('/mypage/update', 'UserController@user_binding')->middleware('login');
-Route::post('/repassword/{id}', 'UserController@user_repwd');
-Route::post('/user_pwd_update/{id}','UserController@user_pwd_update');
+Route::get('/repassword/{id}', 'UserController@user_repwd');
+Route::post('user_pwd_update/{id}','UserController@user_pwd_update');
 
 // item Controller
 Route::post('/product', 'ItemController@store');
