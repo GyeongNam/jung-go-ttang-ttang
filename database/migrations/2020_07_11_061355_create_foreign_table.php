7ab2f33d8ee2ed3be6a776790c61ff5537a34170
Lee Myeongki <leemk2448@naver.com>
2020-06-18 11:20:17 +0900

    데베수정

--- database/migrations/2020_07_11_061355_create_foreign_table.php
+++ database/migrations/2020_07_11_061355_create_foreign_table.php
@@ -13,40 +13,47 @@ class CreateForeignTable extends Migration
      */
     public function up()
     {
-          Schema::table('items', function (Blueprint $table) {
+            Schema::table('items', function (Blueprint $table) {
              $table->string('seller_id')->index();
              $table->foreign('seller_id')->references('id')->on('users');
 });
-          Schema::table('auction', function (Blueprint $table) {
+            Schema::table('auction', function (Blueprint $table) {
              $table->unsignedBigInteger('auction_itemnum')->index();
              $table->foreign('auction_itemnum')->references('item_number')->on('items');
 });
-          Schema::table('enditem', function (Blueprint $table) {
+            Schema::table('enditem', function (Blueprint $table) {
              $table->unsignedBigInteger('end_num')->index();
              $table->foreign('end_num')->references('auction_num')->on('auction');
 });
-          Schema::table('favorite', function (Blueprint $table) {
+            Schema::table('favorite', function (Blueprint $table) {
               $table->unsignedBigInteger('favorite_itemnum')->index();
               $table->foreign('favorite_itemnum')->references('item_number')->on('items');
 });
-          Schema::table('chatroom', function (Blueprint $table) {
+            Schema::table('chatroom', function (Blueprint $table) {
               $table->string('user1_ID')->index();
               $table->foreign('user1_ID')->references('id')->on('users');
 
               $table->string('user2_ID')->index();
               $table->foreign('user2_ID')->references('id')->on('users');
 });
-          Schema::table('message', function (Blueprint $table) {
+            Schema::table('message', function (Blueprint $table) {
               $table->integer('chat_num')->index();
               $table->foreign('chat_num')->references('chatroom_num')->on('chatroom');
-            });
-            Schema::table('comment',function (Blueprint $table){
+});
+            Schema::table('comment', function (Blueprint $table) {
               $table->string('comment_id')->index();
               $table->foreign('comment_id')->references('id')->on('users');
-              
-              $table->integer('comm_item')->index();
+
+              $table->bigIncrements('comm_item')->index();
               $table->foreign('comm_item')->references('item_number')->on('items');
-            });
+});
+            Schema::table('authority', function (Blueprint $table) {
+              $table->string('manage_id')->index();
+              $table->foreign('manage_id')->references('id')->on('users');
+
+              $table->unsignedBigInteger('successful_price')->index();
+              $table->foreign('successful_price')->references('num')->on('enditem');
+});
     }
 
     /**
@@ -58,10 +65,11 @@ class CreateForeignTable extends Migration
     {
         Schema::table('items',    function (Blueprint $table){});
         Schema::table('auction',  function (Blueprint $table){});
-        Schema::table('enditem', function (Blueprint $table){});
+        Schema::table('enditem',  function (Blueprint $table){});
         Schema::table('favorite', function (Blueprint $table){});
         Schema::table('chatroom', function (Blueprint $table){});
         Schema::table('messege',  function (Blueprint $table){});
-        Schema::table('comment', function (Blueprint $table){});
+        Schema::table('comment',  function (Blueprint $table){});
+        Schema::table('authority',function (Blueprint $table){});
     }
 }
