c5e535830608851adec6e7cdd40c4ea19f276d64
dorumamu <64189518+dorumamu@users.noreply.github.com>
2020-06-20 14:02:29 +0900

    메인페이지 이동 수정

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 // 페이지 이동
-Route::get('/main', function (){
-  return view('main');
-});
+// Route::get('/main', function (){
+//   return view('main');
+// });
 Route::get('/Login', function () {
     return view('login.login');
 });
@@ -78,7 +78,7 @@ Route::get('/mypage/update', 'UserController@user_binding')->middleware('login')
 Route::get('/repassword/{id}', 'UserController@user_repwd');
 Route::post('/user_pwd_update/{id}','UserController@user_pwd_update');
 Route::get('/Servicecenter','UserController@qna');
-Route::get('/main','ItemController@mainview');
+Route::get('/','ItemController@mainview');
 
 
 
