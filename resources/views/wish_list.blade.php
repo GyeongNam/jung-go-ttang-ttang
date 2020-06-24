277f6aef78a6754df6ff8a6e576ab955172d9983
dorumamu <272518@naver.com>
2020-06-24 15:16:52 +0900

    위시리스트 삭제

--- resources/views/wish_list.blade.php
+++ resources/views/wish_list.blade.php
@@ -6,19 +6,6 @@
 
 @section('js')
 <script type="text/javascript">
-$(function(){
-  $('#sel_delbt').click(function(){
-    var result = confirm('선택된 항목을 관심품목에서 삭제하시겠습니까?'); // 관심항목 삭제를 위한 선택창을 띄운다.
-
-    if(result) {
-      //맞으면 아이템 삭제 (기능구현중)
-      location.replace('/wish_lst');
-    }else {
-      //취소하면 페이지 그대로 유지
-    }
-  });
-});
-
 $(document).ready(function(){
   //최상단 체크박스 클릭
   $("#checkall").click(function(){
@@ -33,7 +20,21 @@ $(document).ready(function(){
     }
   })
 })
+</script>
+<script type="text/javascript">
+$(function(){
+  $('#sel_delbt').click(function(){
 
+    var result = confirm('선택된 항목을 관심품목에서 삭제하시겠습니까?'); // 관심항목 삭제를 위한 선택창을 띄운다.
+
+    if($(".chackSelect").prop("checked")) {
+      //맞으면 아이템 삭제 (기능구현중)
+      location.replace('/wish_lst');
+    }else {
+      //취소하면 페이지 그대로 유지
+    }
+  });
+});
 </script>
 
 @endsection
@@ -50,12 +51,20 @@ $(document).ready(function(){
         <div class="wish_btn">
           <div class="w_chack">
             <label>전체선택<input type="checkbox" id="checkall" /></label>
-            <button class="sel_del" id="sel_delbt"type="button" name="button" onclick="delete_select">선택 삭제</button>
-            <div class="hyper">
+            {{-- @if(session()->has('login_ID'))
+                @if(decrypt(session()->get('login_ID')) == $value->favorite_id) --}}
+                <div class="action_remove_item">
+                    {{-- <a> --}}
+                      <button class="sel_del" id="sel_delbt"type="button" name="button" onclick="delete_select">선택 삭제</button>
+                    {{-- </a> --}}
+                </div>
+            {{-- @endif
+          @endif --}}
+            {{-- <div class="hyper">
               <a href="#">최신순</a>
               <a href="#">높은가격순</a>
               <a href="#">낮은가격순</a>
-            </div>
+            </div> --}}
           </div>
         </div>
 
@@ -63,13 +72,11 @@ $(document).ready(function(){
       <div class="wish_block">
             <div class="wish_info">
               @foreach($wish_item as $key => $value)
-              <div class="wish_item_info">
-                <a href="/product-detail/{{$value->item_number}}">
-                <div class="hreftem" OnClick="location.href ='/' " style="cursor:pointer;" >
+              <div class="wish_item_info" value="{{$value->item_number}}">
+                <a href="/product-detail/{{$value->item_number}}" >
+                <div class="hreftem" style="cursor:pointer;" >
                   <img id="itmg" src="/img/item/{{$value->item_picture}}" alt="">
                   <div class="whghltn">
-                <!-- <a href="#" class="prdt_link"> -->
-                  <!-- <div class="wish_item_img"> -->
                <input type="checkbox" name="chk" class="chackSelect" value="">
                   </div>
                   <div class="w_i_info">
@@ -80,6 +87,9 @@ $(document).ready(function(){
                       <div class="wiproprice">
                         가격:{{number_format($value->item_startprice)}}원
                       </div>
+                      <a href="/wishitem_remove/{{$value->favorite_itemnum}}/{{$value->favorite_name}}">
+                        <button class="del_jjim" type="button" name="button">삭제하기</button>
+                      </a>
                     </div>
                   </div>
                 </div>
