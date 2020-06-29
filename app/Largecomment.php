c0b0fb20cb062a426e4a9aed0af154b8fe5ff1ef
dorumamu <272518@naver.com>
2020-06-29 13:22:06 +0900

    별거없음

--- app/Largecomment.php
+++ app/Largecomment.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Largecomment extends Model
 {
+    protected $table = 'largecomment';
+    protected $fillable =[
+      'largecomment_num',
+      'largetime',
+      'largecomments',
+      'largecomment_id',
+      'largecomm_item'
+    ];
     //
 }
