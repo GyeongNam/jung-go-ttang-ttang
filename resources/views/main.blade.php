a459afae6d4f5f3d074061576b3267b0cc77a4d2
dorumamu <272518@naver.com>
2020-07-03 17:44:34 +0900

    dfdf

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -18,25 +18,48 @@
     var mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = {
       center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-      level: 3 // 지도의 확대 레벨
+      level: 7 // 지도의 확대 레벨
     };
 
     // 지도를 생성합니다
     var map = new kakao.maps.Map(mapContainer, mapOption);
 
+    // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+    var mapTypeControl = new kakao.maps.MapTypeControl();
+
+    // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+    // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+    map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+    // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+    var zoomControl = new kakao.maps.ZoomControl();
+    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+    // // 마커 클러스터러를 생성합니다
+    // var clusterer = new kakao.maps.MarkerClusterer({
+    //     map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
+    //     averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+    //     minLevel: 10 // 클러스터 할 최소 지도 레벨
+    // });
     // 주소-좌표 변환 객체를 생성합니다
     var geocoder = new kakao.maps.services.Geocoder();
-
+    // var contentString	=
+    // '<div>' +
+    // '<h2>직거래 위치:</h2>'+
+    // '<h3>'+address+'</h3>'+
+    // '<p>'+id+'님의. 직거래 위치입니다!</p>' +
+    // '</div>';
     var positions = $('.maparry');
+    var rodcount = {{count($road)}};
+    console.log(rodcount);
     var add = [];
-    // console.log(add);
-    // console.log(positions[1]);
     for(i=0; i<positions.length;i++){
       add.push(positions[i].value);
     }
     console.log(add);
     // 주소로 좌표를 검색합니다
     for(i=0; i <add.length; i++){
+      console.log(add[i]);
       geocoder.addressSearch(add[i], function(result, status) {
 
         // 정상적으로 검색이 완료됐으면
@@ -52,7 +75,7 @@
 
           // 인포윈도우로 장소에 대한 설명을 표시합니다
           var infowindow = new kakao.maps.InfoWindow({
-            content: '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>'
+            content: '<div style="width:150px;text-align:center;padding:6px 0;">'+ add[i] +'</div>'
           });
           infowindow.open(map, marker);
 
@@ -164,7 +187,11 @@
           @foreach($road as $key => $value)
             <input  id="maparry{{$value->item_number}}" class ="maparry" type="hidden"  value="{{$value->roadAddress}}">
           @endforeach
-          <div id="map" style="width:auto; height:300px;"></div>
+          <div id="map" style="width:auto; height:300px;">
+            <div class="wa d">
+              <a href="#" class="hi"></a>
+            </div>
+          </div>
           <!--제품 정보 표시-->
           <div class="product">
             <div class="categorybar">
