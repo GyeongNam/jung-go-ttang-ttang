7687c05368803ab819b121836de16d3ace280f98
dorumamu <272518@naver.com>
2020-07-07 17:13:13 +0900

    지오로케이션에 반경값을 넣음 이제 반경내에만 마커를 추가해야함...

--- resources/views/mylocation.blade.php
+++ resources/views/mylocation.blade.php
@@ -46,11 +46,24 @@
         lon = position.coords.longitude; // 경도
 
         var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-        message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
+        message = '<div style="padding:5px;">현재 사용자의 위치입니다?!</div>'; // 인포윈도우에 표시될 내용입니다
 
         // 마커와 인포윈도우를 표시합니다
         displayMarker(locPosition, message);
+        // 지도에 표시할 원을 생성합니다
+        var circle = new kakao.maps.Circle({
+          center :locPosition,  // 원의 중심좌표 입니다
+          radius: 1000, // 미터 단위의 원의 반지름입니다
+          strokeWeight: 5, // 선의 두께입니다
+          strokeColor: '#75B8FA', // 선의 색깔입니다
+          strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+          strokeStyle: 'dashed', // 선의 스타일 입니다
+          fillColor: '#CFE7FF', // 채우기 색깔입니다
+          fillOpacity: 0.7  // 채우기 불투명도 입니다
+        });
 
+        // 지도에 원을 표시합니다
+        circle.setMap(map);
       });
 
     } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
@@ -100,6 +113,7 @@
     var mappicture = $('.mappicture');
     var mapstartprice = $('.mapstartprice')
     var mapproduct = $('.mapproduct');
+
     console.log(mapproduct);
     var rodcount = {{count($road)}};
     // console.log(id);
@@ -146,12 +160,11 @@
             map: map,
             position: coords
           });
+
           var link = mapproduct[i];
           var fuck = picture[i];
           console.log(fuck);
           // 마커에 커서가 오버됐을 때 마커 위에 표시할 인포윈도우를 생성합니다
-          // var iwContent =
-
           // iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
           var iwContent =
           '<div class="wrap">' +
