1e24ccdffda36889e9116ce5a477b7f64ecdf0a3
dorumamu <64189518+dorumamu@users.noreply.github.com>
2020-07-26 12:26:34 +0900

    상품 등록사진들 및 레이아웃 바 수정 및 지도 마커 변경

--- resources/views/mylocation.blade.php
+++ resources/views/mylocation.blade.php
@@ -50,8 +50,10 @@ $(document).ready(function(){
       let lat = position.coords.latitude, // 위도
       lon = position.coords.longitude; // 경도
 
-      var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-      message = '<div class="locationmy">현재 사용자의 위치입니다</div>'; // 인포윈도우에 표시될 내용입니다
+      // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+      var locPosition = new kakao.maps.LatLng(lat, lon),
+      // 인포윈도우에 표시될 내용입니다
+      message = '<div class="locationmy">현재 사용자의 위치입니다</div>';
 
       // 마커와 인포윈도우를 표시합니다
       displayMarker(locPosition, message);
@@ -67,11 +69,17 @@ $(document).ready(function(){
 
   // 지도에 마커와 인포윈도우를 표시하는 함수입니다
   function displayMarker(locPosition, message) {
-
+    // 마커 이미지의 이미지 주소입니다
+    var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+    // 마커 이미지의 이미지 크기 입니다
+    var imageSize = new kakao.maps.Size(24, 35);
+    // 마커 이미지를 생성합니다
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
     // 마커를 생성합니다
     var markers = new kakao.maps.Marker({
       map: map,
-      position: locPosition
+      position: locPosition,
+      image : markerImage // 마커 이미지
     });
 
     var iwContent = message, // 인포윈도우에 표시할 내용
